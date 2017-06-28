9520e1778df298d6bf70285dfcf44a1314a689a0
marcila <arcilamatt@outlook.com>
2017-06-28 09:40:12 -0400

    redux-commit

---
+++ src/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import registerServiceWorker from './registerServiceWorker';
+import './index.css';
+
+import App from './components/app';
+import reducers from './reducers';
+
+const createStoreWithMiddleware = applyMiddleware()(createStore);
+
+ReactDOM.render(
+  <Provider store={createStoreWithMiddleware(reducers)}>
+    <App />
+  </Provider>
+  ,document.getElementById('root'));
+registerServiceWorker();
+
+
+
+//the most parent Component revieces the data. Downstream data
---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function() {
+  return [
+    {title: 'Javascript: The Goof Parts', pages: 101},
+    {title: 'Harry Potter', pages: 39},
+    {title: 'The Dark Tower', pages: 85},
+    {title: 'Elquent Ruby', pages: 1}
+  ]
+}
